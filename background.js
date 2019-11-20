cbe3f301949c7a4b5435d5c247a598ee11beba8a
LilyChen <lily.chen@liveramp.com>
2019-11-20 11:27:02 +0800

    replace uuid to did

--- background.js
+++ background.js
@@ -1,13 +1,32 @@
 'use strict';
-var cookies_name = "DFTT_END_USER_PREV_BOOTSTRAPPED";
-var cookies_url = "https://liveramp.com/";
+var didAuth = require('@decentralized-identity/did-auth-jose');
+
+var cookies_name = "APID";
+var cookies_url = "https://advertising.com/";
 var extension_server = "http://localhost:8081/extension/match";
-var code_segment = "https://liveramp.com/wp-admin/admin-ajax.php";
+var code_segment = "<link rel=\"publisher\" href=\"https://plus.google.com/+zulily\"";
 
 chrome.runtime.onInstalled.addListener(function () {
-    let uuid = this.uuid();
-    chrome.storage.sync.set({"DID": uuid, "key": uuid + "key"}, function () {
-        alert("Did is " + uuid);
+    // let uuid;
+    // chrome.storage.sync.get('DID', function (data) {
+    //     let stored_uuid = data.DID;
+    //     if (stored_uuid) {
+    //         uuid = stored_uuid;
+    //     } else {
+    //         uuid = this.uuid();
+    //     }
+    //     chrome.storage.sync.set({"DID": uuid, "key": uuid + "key"}, function () {
+    //         alert("Did is " + uuid);
+    //     });
+    // });
+
+    chrome.storage.sync.get('DID', function (data) {
+        let stored_uuid = data.DID;
+        if (stored_uuid) {
+            uuid = stored_uuid;
+        } else {
+            createDid();
+        }
     });
 });
 
@@ -61,6 +80,76 @@ function uuid() {
     s[8] = s[13] = s[18] = s[23] = "-";
     var uuid = s.join("");
     return uuid;
+}
+
+async function createJwk() {
+    const kid = '#key-1';
+    const privKey = await didAuth.EcPrivateKey.generatePrivateKey(kid);
+    const pubKey = privKey.getPublicKey();
+    pubKey.defaultSignAlgorithm = 'ES256K';
+
+    return {"jwkPriv": privKey, "jwkPub": pubKey};
+}
+
+async function createDid() {
+    const jwk = await createJwk();
+    const jwkPriv = jwk.jwkPriv;
+    const jwkPub = jwk.jwkPub;
+    const kid = '#key-1';
+    const privKey = await didAuth.EcPrivateKey.generatePrivateKey(kid);
+    const pubKey = privKey.getPublicKey();
+    pubKey.defaultSignAlgorithm = 'ES256K';
+
+    const privateKey = didAuth.EcPrivateKey.wrapJwk(jwkPriv.kid, jwkPriv);
+
+    const body = {
+        "@context": "https://w3id.org/did/v1",
+        publicKey: [
+            {
+                id: jwkPub.kid,
+                type: "Secp256k1VerificationKey2018",
+                publicKeyJwk: jwkPub
+            }
+        ],
+        service: [
+            {
+                id: "IdentityHub",
+                type: "IdentityHub",
+                serviceEndpoint: {
+                    "@context": "schema.identity.foundation/hub",
+                    "@type": "UserServiceEndpoint",
+                    instance: [
+                        "did:test:hub.id",
+                    ]
+                }
+            }
+        ],
+    };
+
+    const header = {
+        alg: jwkPub.defaultSignAlgorithm,
+        kid: jwkPub.kid,
+        operation:'create',
+        proofOfWork:'{}'
+    };
+
+    const cryptoFactory = new didAuth.CryptoFactory([new didAuth.Secp256k1CryptoSuite()]);
+    const jwsToken = new didAuth.JwsToken(body, cryptoFactory);
+    const signedBody = await jwsToken.signAsFlattenedJson(privateKey, {header});
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://beta.ion.microsoft.com/api/1.0/register", true);
+    xhr.setRequestHeader("Content-type","application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            var resp = JSON.parse(xhr.responseText);
+            let did = resp.id;
+            chrome.storage.sync.set({"DID": did}, function () {
+                alert(did);
+            });
+        }
+    }
+    xhr.send(JSON.stringify(signedBody));
 }
 
 function onWindowLoad() {
